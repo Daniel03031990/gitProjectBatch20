6ab15b8ae54347dffd492ed9302dfe74bbb45d91
Daniel03031990 <grachiladaniel@gmail.com>
2024-08-11 17:59:42 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import org.w3c.dom.ls.LSOutput;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println();
+        System.out.println("Hello day");
     }
 
 }
